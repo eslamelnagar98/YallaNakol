c1aac2ed8a7111a29472330980fb6c5111b6832f
Ahmed Nagdy <ahmed.nagdy39@gmail.com>
2021-02-20 22:32:42 +0200

    restaurant controller added

--- YallaNakol.Data/Services/IRestaurant.cs
+++ YallaNakol.Data/Services/IRestaurant.cs
@@ -8,6 +8,10 @@ namespace YallaNakol.Data.Services
     public interface IRestaurant
     {
         IEnumerable<Restaurant> AllRestaurants { get; }
-        Restaurant GetRestaurantById(int restaurantId);
+        Restaurant GetRestaurantById(int? restaurantId);
+        void AddRestaurant(Restaurant restaurant);
+        void UpdateRestaurant(Restaurant restaurant);
+        void DeleteRestaurant(Restaurant restaurant);
+        bool RestaurantExists(int id);
     }
 }
--- YallaNakol.UI/Controllers/DishesController.cs
+++ YallaNakol.UI/Controllers/DishesController.cs
@@ -6,96 +6,102 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using YallaNakol.Data.Models;
-using YallaNakol.Data.Services;
 
 namespace YallaNakol.UI.Controllers
 {
-    public class RestaurantsController : Controller
+    public class DishesController : Controller
     {
-        private readonly IRestaurant _repo;
+        private readonly ApplicationDbContext _repo;
 
-        public RestaurantsController(IRestaurant context)
+        public DishesController(ApplicationDbContext context)
         {
             _repo = context;
         }
 
-        public IActionResult Index()
+        // GET: Dishes
+        public async Task<IActionResult> Index()
         {
-            return View(_repo.AllRestaurants);
+            return View(await _repo.Dishes.ToListAsync());
         }
-        
-        // GET: Restaurants/Details/5
-        public IActionResult Details(int? id)
+
+        // GET: Dishes/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-            var restaurant = _repo.GetRestaurantById(id);
-            if (restaurant == null)
+
+            var dish = await _repo.Dishes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
             {
                 return NotFound();
             }
-            return View(restaurant);
+
+            return View(dish);
         }
-        
-        // GET: Restaurants/Create
+
+        // GET: Dishes/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Restaurants/Create
+        // POST: Dishes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Id,Name,Address,Description,Rate,ImageUrl,PhoneNumber,WorkingHours,DeliveryAreas")] Restaurant restaurant)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,ImageUrl,InStock")] Dish dish)
         {
             if (ModelState.IsValid)
             {
-                _repo.AddRestaurant(restaurant);
+                _repo.Add(dish);
+                await _repo.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(restaurant);
+            return View(dish);
         }
-        
-        // GET: Restaurants/Edit/5
-        public IActionResult Edit(int? id)
+
+        // GET: Dishes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var restaurant = _repo.GetRestaurantById(id);
-            if (restaurant == null)
+            var dish = await _repo.Dishes.FindAsync(id);
+            if (dish == null)
             {
                 return NotFound();
             }
-            return View(restaurant);
+            return View(dish);
         }
-        
-        // POST: Restaurants/Edit/5
+
+        // POST: Dishes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, [Bind("Id,Name,Address,Description,Rate,ImageUrl,PhoneNumber,WorkingHours,DeliveryAreas")] Restaurant restaurant)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,ImageUrl,InStock")] Dish dish)
         {
-            if (id != restaurant.Id)
+            if (id != dish.Id)
             {
                 return NotFound();
             }
+
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _repo.UpdateRestaurant(restaurant);
+                    _repo.Update(dish);
+                    await _repo.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!_repo.RestaurantExists(restaurant.Id))
+                    if (!DishExists(dish.Id))
                     {
                         return NotFound();
                     }
@@ -106,34 +112,41 @@ namespace YallaNakol.UI.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(restaurant);
+            return View(dish);
         }
-        
-        // GET: Restaurants/Delete/5
-        public IActionResult Delete(int? id)
+
+        // GET: Dishes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var restaurant = _repo.GetRestaurantById(id);
-            if (restaurant == null)
+            var dish = await _repo.Dishes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
             {
                 return NotFound();
             }
 
-            return View(restaurant);
+            return View(dish);
         }
 
-        // POST: Restaurants/Delete/5
+        // POST: Dishes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var restaurant = _repo.GetRestaurantById(id);
-             _repo.DeleteRestaurant(restaurant);
+            var dish = await _repo.Dishes.FindAsync(id);
+            _repo.Dishes.Remove(dish);
+            await _repo.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool DishExists(int id)
+        {
+            return _repo.Dishes.Any(e => e.Id == id);
         }
     }
 }

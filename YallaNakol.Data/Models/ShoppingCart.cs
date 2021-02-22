f35115ce7e06d09fa0c87437d5c49975cb58d1fa
eslamelnagar98 <40874274+eslamelnagar98@users.noreply.github.com>
2021-02-23 00:28:10 +0200

    A7A E7na Hnhzr

---
+++ YallaNakol.Data/Models/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YallaNakol.Data.Models
+{
+    public class ShoppingCart
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public string CartId { get; set; }
+        private  ShoppingCart(ApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider serviceProvider)
+        {
+            ISession session = serviceProvider.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            var context = serviceProvider.GetService<ApplicationDbContext>();
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+
+
+        }
+    }
+}
---
+++ YallaNakol.Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace YallaNakol.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+                                        
+        public virtual Dish Dish { get; set; }
+
+        [Range(0,int.MaxValue,ErrorMessage ="Amount Cant not be Less Than one")]
+        public int Amount { get; set; }
+
+        [Required]
+        public string ShoppingCartId { get; set; }
+
+
+    }
+}

43e548d0836738d2aedc63d67adc14725d09d440
eslamelnagar98 <40874274+eslamelnagar98@users.noreply.github.com>
2021-02-24 03:09:57 +0200

    Add Application User Class

---
+++ YallaNakol.Data/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YallaNakol.Data.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName ="nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ YallaNakol.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YallaNakol.Data.Models;
+
+[assembly: HostingStartup(typeof(YallaNakol.UI.Areas.Identity.IdentityHostingStartup))]
+namespace YallaNakol.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseSqlServer(
+                       context.Configuration.GetConnectionString("MyConn")));
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                        .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}

55b42494d2d1249d35a843734be04a29d0fb8b12
Ahmed Nagdy <ahmed.nagdy39@gmail.com>
2021-02-27 22:31:24 +0200

    Map JS done
    pending MVC integration

--- YallaNakol.UI/wwwroot/js/Map.js
+++ YallaNakol.UI/wwwroot/js/Map.js
@@ -1,53 +1,67 @@
 ﻿
-var Mymap = document.querySelector('#map')
-function loadMap() {
-    console.log("not working");
-    //check browser support
-    if (navigator.geolocation) {
-        //get position
+var mapHTML = document.querySelector('#map')
+var myLat = 30;
+var myLng = 31;
+var address = { zone: 0, area: 0, city: 0 }
+var myMap;
 
-        navigator.geolocation.getCurrentPosition(success, error)
-    }
-    else {
-        map.innerText = "update your browser :P"
-    }
+getPosition();
 
-}
-    loadMap();
-
-function success(e) {
-    console.log(e)
-    // map.innerText = "position is "+e.coords.latitude + " , " +e.coords.longitude
-    var myMap = new google.maps.Map(Mymap, {
-        center: {
-            lat: e.coords.latitude,
-            lng: e.coords.longitude
-        },
-        zoom: 16
+function getPosition()
+{
+    navigator.geolocation.getCurrentPosition(function (e) {
+        
+        myLat = e.coords.latitude;
+        myLng = e.coords.longitude;
+        //---------------
+        myMap = new google.maps.Map(mapHTML, {
+            center: {
+                lat: myLat,
+                lng: myLng
+            },
+            zoom: 16
+        })
+        geocode();
+        AllowDrag();
+    }, function ()
+    {
+            console.log("Error getting location");
+            AllowDrag();
     })
-
 }
 
-function error(e) {
-    console.log(e)
-    var message = "";
-    switch (e.code) {
-        case 1: {
-            message = "please allow me to know your location"
-            break
-        }
-        case 2: {
-            message = "position is not available check your configuration"
-            break
-        }
-        case 3: {
-            message = "try again later"
-            break
-        }
-        default: {
-            message = "unknown error"
-        }
-
-    }
-    map.innerText = message
+function geocode()
+{
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(myLat, myLng);
+    geocoder.geocode({ location: latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK)
+        {
+            //Loop to get the result with only four components
+            for (var i = 0; i < results.length; i++)
+            {
+                console.log(results[i].types[0])
+                if (results[i].types[0] == "administrative_area_level_3")
+                {
+                    var addressComponents = results[i].address_components;
+                    address.zone = addressComponents[0].short_name;
+                    address.area = addressComponents[1].short_name;
+                    address.city = addressComponents[2].short_name.split(' ')[0];
+                    break;
+                }
+            }
+        };
+    });
 }
+function AllowDrag() {
+    google.maps.event.addListener(myMap, 'center_changed',
+        function () {
+            myLat = myMap.getCenter().lat();
+            myLng = myMap.getCenter().lng();
+            geocode();
+        });
+}
+
+
+
+

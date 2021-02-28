ce9f079027567bdacd3fbecc2a8968a0ff57ad00
eslamelnagar98 <40874274+eslamelnagar98@users.noreply.github.com>
2021-02-28 18:23:40 +0200

    Add Shopping Cart Icon

---
+++ YallaNakol.UI/Component/ShoppingCartIconViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YallaNakol.Data.Models;
+using YallaNakol.Data.Services;
+
+namespace YallaNakol.UI.Component
+{
+    public class ShoppingCartIconViewComponent:ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartIconViewComponent(IShoppingCart shoppingCart)
+        {
+            this._shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_shoppingCart.ShoppingCartItems.Count());
+        }
+    }
+}
